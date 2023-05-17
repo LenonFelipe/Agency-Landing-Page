d6fca9885bc041930a78235f98c0879a5db86bf0
LenonFelipe <lenonfelipe98@gmail.com>
2023-05-17 15:40:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function menuDown(){
+    let menuMobile = document.getElementById("menu-mobile");
+    menuMobile.style.top = "80px"
+}
+function menuUp(){
+    let menuMobile = document.getElementById("menu-mobile");
+    menuMobile.style.top = "-480px"
+}
